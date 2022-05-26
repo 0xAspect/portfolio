37ed9f9063a3bc0a5ba509ed1ae97f176bb5c648
Dev <devdevavax@gmail.com>
2022-05-26 10:06:58 +0200

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./navbar.scss";
+import { FaBars } from 'react-icons/fa';
+import { HiCubeTransparent as HiCube } from 'react-icons/hi';
+
+
+const Navbar = () => {
+  return (
+    <>
+      <nav class="nav">
+        
+        <div class='nav-link title'>0xAspect <HiCube className="icon" /></div>
+        <ul class="nav-menu">
+          <li class='nav-link'>Projects</li>
+          <li class='nav-link'>About me</li>
+          <li class='nav-link'>Contact</li>
+        </ul>
+        <div class ="bars"><FaBars className='burger-menu' /></div>
+
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
