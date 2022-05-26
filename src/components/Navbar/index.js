61cfa35fdf5a68e7f9e827b0c8f3498b55f71155
Dev <devdevavax@gmail.com>
2022-05-26 21:36:38 +0200

    next commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./navbar.scss";
 import { FaBars } from 'react-icons/fa';
 import { HiCubeTransparent as HiCube } from 'react-icons/hi';
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
+  const [expand, setExpand] = useState(false);
+
+  const handleClick = () => {
+    setExpand(!expand);
+  }
+
   return (
     <>
       <nav class="nav">
         
         <div class='nav-link title'>0xAspect <HiCube className="icon" /></div>
-        <ul class="nav-menu">
-          <li class='nav-link'>About Me</li>
-          <li class='nav-link'>Projects</li>
+        <div class="nav-menu">
+          <NavLink className="nav-link" to="/">About Me</NavLink>
+          <NavLink className="nav-link" to="/projects">Projects</NavLink>
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
           <li class='nav-link'>Links</li>
-          <li class='nav-link'>Contact</li>
-
-        </ul>
-        <div class ="bars"><FaBars className='burger-menu' /></div>
+        </div>
+        <div class ="bars" onClick={handleClick}><FaBars className='burger-menu' /></div>
       </nav>
     </>
   );
