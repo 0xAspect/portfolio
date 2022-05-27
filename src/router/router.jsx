d7d779a70013f8755a0db3d693dde4d88021bf09
Dev <devdevavax@gmail.com>
2022-05-28 00:19:36 +0200

    name edit

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home/Home";
-import Navbar from "../components/Navbar/index.js";
+import Navbar from "../components/Navbar/Navbar";
 import Projects from "../components/Projects/Projects";
 import Contact from "../components/Contact/Contact";
 
