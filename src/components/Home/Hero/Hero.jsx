52045f85ec2c4a88fd7ff1b692c2276a4246bfc3
Dev <devdevavax@gmail.com>
2022-06-04 18:33:31 +0200

    projects scroll functionality

---
+++ src/components/Home/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Video from '../../../videos/video.mp4'
+import  "./hero.scss"
+import { Link as SLink, animateScroll as scroll } from "react-scroll";
+
+const Hero = () => {
+  return (
+    <>
+    <div className='hero-container'>
+      <div className='hero-bg'>
+        <video className ="video-bg" autoPlay loop muted src ={Video} type='video/mp4' />
+        </div>
+        <div className='hero-content'>
+          <h1 className='hero-h1'>Solidity/Web3.js Developer </h1>
+          <p className='hero-p'>
+            Passionate about Web3, NFT and DeFi with previous production-level smart contract building and deployment experience. 
+          </p>
+          <p className='hero-p'>Looking to move into the blockchain space full-time.</p>
+          <SLink     smooth={true}  duration={500} to= "section1" className='main-button'>Projects </SLink>
+        </div>
+      </div>
+      </>
+  )
+}
+
+export default Hero;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ import  "./home.scss"
 import { Link as SLink, animateScroll as scroll } from "react-scroll";
 
 const Home = () => {
+
+  
   return (
     <>
     <div className='hero-container'>
@@ -13,13 +15,19 @@ const Home = () => {
         <div className='hero-content'>
           <h1 className='hero-h1'>Solidity/Web3.js Developer </h1>
           <p className='hero-p'>
-            Passionate about Web3, NFT and DeFi with previous production-level smart contract building and deployment experience. 
+            Passionate about Web3, NFTs and DeFi with previous full-stack production-level DApp building and deployment experience. 
           </p>
-          <p className='hero-p'>Looking to move into the blockchain space full-time.</p>
+          <p className='hero-p'>Looking for development opportunities in the blockchain space.</p>
           <SLink     smooth={true}  duration={500} to= "section1" className='main-button'>Projects </SLink>
         </div>
       </div>
-      <div title="section1" id="section1" className="regular-container"> asdasd
+      <div title="section1" id="section1" className="regular-container"> 
+      <div className='hero-content'><h1 className='projects-h1'> Solidity </h1>
+      <div className='category-preview'> asndaskdaskdas</div>
+        <h1 className='projects-h1'> React/Web3.js </h1>
+
+</div>
+
       </div>
   
       </>
