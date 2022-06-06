343543a09a1287c9dfad7e7edbe5ecb415a78128
Dev <devdevavax@gmail.com>
2022-06-06 15:19:20 +0200

    projects viewer build

--- src/components/Projects/ProjectsViewer.jsx
+++ src/components/Projects/ProjectsViewer.jsx
@@ -19,15 +19,28 @@ const ProjectsViewer = () => {
           <a className= 'header-link' href={projectData.links.github}>GitHub</a>
           <a className= 'header-link' href={projectData.links.blockscanner}>Block Explorer</a>
           <a className= 'header-link' href={projectData.links.frontend}>Front End</a>
-          </div></div>
+          </div>
+          </div>
         <hr/>
+        
         <ul className='display-tech'>{projectData.tech.map(tool => { return (<li className='display-tech-item'>{tool + " "} </li> )})}</ul>
 
         <div className='display-images-container'>
         <img className='display-img' src={require('../../images/Projects/'+projectData.url.toString()+'/0.jpg')}></img>
         <img className='display-img'src={require('../../images/Projects/'+projectData.url.toString()+'/1.jpg')}></img>
         <img className='display-img'src={require('../../images/Projects/'+projectData.url.toString()+'/3.jpg')}></img>
-        <p>{projectData.description}</p>
+        <img className='display-img'src={require('../../images/Projects/'+projectData.url.toString()+'/4.jpg')}></img>
+
+        <h1 className='body-title'>Description</h1>
+        {projectData.description.map((value, index) => {
+        return <p className='description-p' key={index}>{value}</p>
+      })}
+      <h1 className='body-title' >Features</h1>
+      <p className='description-p'>{projectData.features.description}</p>
+      <ul>      {projectData.features.features.map((value, index) => {
+        return <li className="features-list-item" key={index}>{value}</li>
+      })}</ul>
+
 
         </div>
       </div>
