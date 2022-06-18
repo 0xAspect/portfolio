71beea0da37c500026900b7564b0b3019a5c9e13
Dev <devdevavax@gmail.com>
2022-06-18 18:00:56 +0200

    title

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -13,9 +13,9 @@ const Hero = () => {
         <div className='hero-content'>
           <h1 className='hero-h1'>Solidity/Web3.js Developer </h1>
           <p className='hero-p'>
-            Passionate about Web3, NFT and DeFi with previous production-level smart contract building and deployment experience. 
+            Passionate about Web3, NFT and DeFi with previous production-level smart contract design, build and deployment experience. 
           </p>
-          <p className='hero-p'>Looking to move into the blockchain space full-time.</p>
+          <p className='hero-p'>Looking for opportunities in theb blockchain space.</p>
           <SLink     smooth={true}  offset={-70} duration={500} to= "projects" className='main-button'>Projects </SLink>
         </div>
       </div>
